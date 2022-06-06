68eb223c388dc02cc416ae488c67aa5becf0b9b1
Andrew Sutherland <andrew.sutherland@live.com>
2022-06-06 10:09:33 +1000

    Added unit test to validate deserialization behaviour of partial xp classes.

--- src/OrderCloud.SDK.Tests/SdkTests.cs
+++ src/OrderCloud.SDK.Tests/SdkTests.cs
@@ -105,6 +105,85 @@ namespace OrderCloud.SDK.Tests
 			}, li);
 		}
 
+		/// <summary>
+		/// Support partial OrderCloud model deserialization.
+		/// Primarily for partial xp where the intention is to reduce xp bloat in OrderCloud, while still enforcing data types in a strongly typed class.
+		/// Complete OrderCloud models aren't necessarily expected to be deserialised as the OrderCloud API always returns the complete model,
+		/// however the xp models share the same class and interface inheritance to support serialization/deserialization.
+		/// </summary>
+		[Test]
+		public void can_deserialize_partial()
+		{
+			// Preparing an OrderCloud model that would be expected to be returned by the get product endpoint.
+			var product = new Product<CustomPartialXP>
+			{
+				Name = "MyProduct",
+				xp = new CustomPartialXP { Foo = "test" },
+				Description = "blah",
+				Inventory = new PartialInventory
+				{
+					QuantityAvailable = 999
+				}
+			};
+
+			var serializedProduct = OrderCloudClient.Serializer.Serialize(product);
+			var deserializedProduct = OrderCloudClient.Serializer.Deserialize<CustomPartialProduct<CustomPartialXP>>(serializedProduct);
+
+			// As the complete model is deserialized, we expect to see all properties be assigned to the Props dictionary.
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.OwnerID)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.DefaultPriceScheduleID)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.AutoForward)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.ID)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.Name)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.Description)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.QuantityMultiplier)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.ShipWeight)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.ShipHeight)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.ShipWidth)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.ShipLength)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.Active)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.SpecCount)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.VariantCount)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.ShipFromAddressID)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.DefaultSupplierID)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.Returnable)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.Inventory)));
+			Assert.IsNotNull(deserializedProduct.Props.ContainsKey(nameof(product.xp)));
+
+			// All properties should be mapped, including default values.
+			Assert.AreEqual(product.OwnerID, deserializedProduct.OwnerID);
+			Assert.AreEqual(product.DefaultPriceScheduleID, deserializedProduct.DefaultPriceScheduleID);
+			Assert.AreEqual(product.AutoForward, deserializedProduct.AutoForward);
+			Assert.AreEqual(product.ID, deserializedProduct.ID);
+			Assert.AreEqual(product.Name, deserializedProduct.Name);
+			Assert.AreEqual(product.Description, deserializedProduct.Description);
+			Assert.AreEqual(product.QuantityMultiplier, deserializedProduct.QuantityMultiplier);
+			Assert.AreEqual(product.ShipWeight, deserializedProduct.ShipWeight);
+			Assert.AreEqual(product.ShipHeight, deserializedProduct.ShipHeight);
+			Assert.AreEqual(product.ShipWidth, deserializedProduct.ShipWidth);
+			Assert.AreEqual(product.ShipLength, deserializedProduct.ShipLength);
+			Assert.AreEqual(product.Active, deserializedProduct.Active);
+			Assert.AreEqual(product.SpecCount, deserializedProduct.SpecCount);
+			Assert.AreEqual(product.VariantCount, deserializedProduct.VariantCount);
+			Assert.AreEqual(product.ShipFromAddressID, deserializedProduct.ShipFromAddressID);
+			Assert.AreEqual(product.DefaultSupplierID, deserializedProduct.DefaultSupplierID);
+			Assert.AreEqual(product.Returnable, deserializedProduct.Returnable);
+
+			// We cannot deserialise a nested partial as the PartialProduct still specifies Inventory as the Inventory OrderCloud model,
+			// however OrderCloud API responses return the full model, so this is more an FYI.
+			//Assert.AreEqual(product.Inventory, deserializedProduct.Inventory);
+			Assert.AreEqual(product.Inventory.QuantityAvailable, deserializedProduct.Inventory.QuantityAvailable);
+
+			// The CustomPartialXP crudely overrides the Object.Equals method as an example, however unboxing the Props dictionary for equality checks may be something we wish to as a future enhancement.
+			Assert.AreEqual(product.xp, deserializedProduct.xp);
+			// Validating a handful
+			Assert.IsTrue(deserializedProduct.xp is CustomPartialXP);
+			Assert.AreEqual(1, deserializedProduct.xp.Props.Count, "Only the 'Foo' property should have been added to the Props dictionary.");
+			Assert.AreEqual(product.xp.Props.Count, deserializedProduct.xp.Props.Count, "Only the 'Foo' property should have been added to the Props dictionary.");
+			Assert.AreEqual(product.xp.Foo, deserializedProduct.xp.Foo);
+			Assert.AreEqual(product.xp.Bar, deserializedProduct.xp.Bar);
+		}
+
 		[Test]
 		public async Task can_provide_alternative_token() {
 			// prove that auth credentials are no longer required like in earlier versions
@@ -282,6 +361,44 @@ namespace OrderCloud.SDK.Tests
 			public int Bar { get; set; }
 		}
 
+		private class CustomPartialXP : OrderCloudModel, IPartial
+		{
+			public string Foo { get => GetProp<string>(nameof(Foo)); set => SetProp<string>(nameof(Foo), value); }
+			public int Bar { get => GetProp<int>(nameof(Bar)); set => SetProp<int>(nameof(Bar), value); }
+
+			// This is only included to unit test the xp comparison
+			public override bool Equals(object xp)
+			{
+				if (!(xp is CustomPartialXP))
+				{
+					return false;
+				}
+
+				var that = xp as CustomPartialXP;
+
+				if (this.Props.Count != that.Props.Count)
+				{
+					return false;
+				}
+
+				foreach (var pair in this.Props)
+				{
+					// We don't plan to unbox the Props, so this crude approach is just to demonstrate deserialising custom partial xp is possible.
+					if (pair.Value.ToString() != that.Props[pair.Key].ToString())
+					{
+						return false;
+					}
+				}
+
+				return this.Foo == that.Foo && this.Bar == that.Bar;
+			}
+
+			public override int GetHashCode()
+			{
+				throw new NotImplementedException();
+			}
+		}
+
 		private class CustomConfigData
 		{
 			public string Foo { get; set; }
@@ -292,6 +409,10 @@ namespace OrderCloud.SDK.Tests
 		class CustomAddress : Address<CustomXP> { }
 		class CustomLineItem : LineItem<CustomXP, LineItemProduct, LineItemVariant, CustomAddress, CustomAddress> { }
 		class CustomOrder : Order<CustomXP, CustomUser, CustomAddress> { }
+		class CustomPartialProduct : PartialProduct<CustomPartialXP> { }
+		class CustomPartialProduct<Txp> : CustomPartialProduct {
+			public new Txp xp { get => GetProp<Txp>("xp"); set => SetProp<Txp>("xp", value); }
+		}
 
 		private static class JsonAssert
 		{
