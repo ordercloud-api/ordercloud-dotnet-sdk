42391591533dc0fffaff9008f2b8e48f80ace15b
Erin <erincdustin@gmail.com>
2025-05-22 07:20:35 -0700

    add PublicKey model and GetPublicKeyAsync method to OrderCloudClient

--- src/OrderCloud.SDK/OAuthModels.cs
+++ src/OrderCloud.SDK/OAuthModels.cs
@@ -48,4 +48,20 @@ namespace OrderCloud.SDK
 		public string refresh_token { get; set; }
 		public string client_secret { get; set; }
 	}
+
+	public class PublicKey : OrderCloudModel
+	{
+		/// <summary>Kty of the public key.</summary>
+		public string kty { get => GetProp<string>("kty"); set => SetProp<string>("kty", value); }
+		/// <summary>N of the public key.</summary>
+		public string n { get => GetProp<string>("n"); set => SetProp<string>("n", value); }
+		/// <summary>E of the public key.</summary>
+		public string e { get => GetProp<string>("e"); set => SetProp<string>("e", value); }
+		/// <summary>Alg of the public key.</summary>
+		public string alg { get => GetProp<string>("alg"); set => SetProp<string>("alg", value); }
+		/// <summary>Use of the public key.</summary>
+		public string use { get => GetProp<string>("use"); set => SetProp<string>("use", value); }
+		/// <summary>Kid of the public key.</summary>
+		public string kid { get => GetProp<string>("kid"); set => SetProp<string>("kid", value); }
+	}
 }
